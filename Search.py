68186fcb1955260472b316f4fcbe794e8746e015
Shogo0607 <soffionam@yahoo.co.jp>
2022-06-28 23:09:38 +0900

    add packages

--- Search.py
+++ Search.py
@@ -43,6 +43,7 @@ if st.sidebar.button("検索開始"):
         now = dt.now()
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')  
         chrome_options.add_experimental_option('excludeSwitches', ['enable-automation', 'enable-logging'])
         print('>処理開始')
         driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
